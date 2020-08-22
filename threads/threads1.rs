1fbeba2c707f6d279bd35cbe157b4482a040d6b4
Lyuha <lyuha@users.noreply.github.com>
2020-08-22 17:02:19 +0900

    Change to Arc::clone

--- threads/threads1.rs
+++ threads/threads1.rs
@@ -15,7 +15,7 @@ struct JobStatus {
 
 fn main() {
     let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
-    let status_shared = status.clone();
+    let status_shared = Arc::clone(&status);
     thread::spawn(move || {
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
